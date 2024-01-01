4109a3ede6e94c866fcf79e62bae7a622f2aaf40
PavanSabani <poorvinuchchi769@gmail.com>
2024-01-01 10:25:31 +0530

    modifiednewpro

--- newpro.c
+++ newpro.c
@@ -2,10 +2,14 @@
 int main(){
 int a;
 int b;
+int c;
 printf("enter the element a\n");
 scanf(%d,&a);
 printf("enter the element b\n");
 scanf(%d,&b);
+printf("enter the element c\n");
+scanf(%d,&c);
+
 printf("sum=%d",a+b);
 return 0;
 }
