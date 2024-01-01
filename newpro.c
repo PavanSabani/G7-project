7a9973a5bbd817a428a37a66fd17211e18595dca
PavanSabani <poorvinuchchi769@gmail.com>
2024-01-01 10:22:30 +0530

    newpro

---
+++ newpro.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int a;
+int b;
+printf("enter the element a\n");
+scanf(%d,&a);
+printf("enter the element b\n");
+scanf(%d,&b);
+printf("sum=%d",a+b);
+return 0;
+}
